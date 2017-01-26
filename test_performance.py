29a0fd6f6572dfa27db4ab067fe7daad259cf3c2
nishikiw <yijin.wang@mail.utoronto.ca>
2017-01-26 16:57:24 -0500

    add python file to test performance

---
+++ test_performance.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import subprocess
+
+KB = 1024
+MB = 1024 * 1024
+sizes = [
+    512,
+    1 * KB,
+    4 * KB,
+    8 * KB,
+    16 * KB,
+    32 * KB,
+    1 * MB,
+    2 * MB,
+    4 * MB
+]
+
+filename = sys.argv[1]
+
+f1 = open("performance_write_blocks_seq.txt", "w")
+f2 = open("performance_write_lines.txt", "w")
+
+for size in sizes:
+    subprocess.run(["write_blocks_seq", filename, str(size)], stdout=f1)
+
+subprocess.run(["write_lines", filename], stdout=f2)
+
+f1.close()
+f2.close()
