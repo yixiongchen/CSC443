ec9d6d593c3f46c7630555b37d29ada64a878e4f
YIXIONG CHEN <jack.chenyixiong@gmail.com>
2017-01-31 10:56:42 -0500

    add worksheet

--- test_performance.py
+++ test_performance.py
@@ -22,10 +22,10 @@ f1 = open("performance_write_blocks_seq.txt", "w")
 f2 = open("performance_write_lines.txt", "w")
 
 for size in sizes:
-    subprocess.run(["write_blocks_seq", filename, str(size)], stdout=f1)
-    subprocess.run(["rm", "records.dat"])
+    subprocess.run(["./write_blocks_seq", filename, str(size)], stdout=f1)
+    subprocess.run(["rm", "./records.dat"])
 
-subprocess.run(["write_lines", filename], stdout=f2)
+subprocess.run(["./write_lines", filename], stdout=f2)
 
 f1.close()
 f2.close()
